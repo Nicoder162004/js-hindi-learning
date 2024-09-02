dcb9fdfc9fbcc1542402b077e956dd92dfcbec04
Nicoder162004 <nikhil84094@gmail.com>
2024-09-03 00:13:32 +0530

    dataypes add

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+// Code readability should be high
+// number => 2 to power 53
+// bigint
+// string => """
+// boolean
+// null => standalone value
+// symbol => unique
+// object
+// Use of type of
+console.log(typeof "Nikhil") // It is used to type the datatype of value
+console.log(typeof "null")
