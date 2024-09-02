0445f6903ac05a9058e4f7f34032ccfdc829fd44
Nicoder162004 <nikhil84094@gmail.com>
2024-09-03 00:24:59 +0530

    conversion basic

---
+++ conversion.js
@@ -0,0 +1,14 @@
+let score ="33abc"
+
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(typeof(value));
+console.log(value);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// 1 => true 
+// 0 => false
+// "any_value" => true
